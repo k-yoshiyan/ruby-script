7a6ec497e69cbb058e0c2b5890bd86ae347e5129
k-yoshiyan <yoshiyan@home.email.ne.jp>
2017-05-30 14:31:22 +0900

    Update afp.rb

--- afp.rb
+++ afp.rb
@@ -5,19 +5,23 @@
 # カレントディレクトリーの下のファイルをafplayで連続再生する
 require 'shellwords'
 path = Dir.pwd
+extensions = [".mp3", ".wav", ".MP3", ".WAV"]
 music_array = []
 
-# ディレクトリーの内容オブジェクト数分ループして処理をし、演奏楽曲の配列を作る
+## ディレクトリーの内容オブジェクト数分ループして処理をし、演奏楽曲ファイル名の配列を作る
 Dir.foreach(path) do |x|
- music_array.push(x)
+### mp3, wavのファイル名のみを選択する　
+  if extensions.include?(File.extname(x))
+    music_array.push(x)
+  end
 end
 
-# 演奏楽曲の配列を個数分ループして処理
+## 演奏楽曲の配列を個数分ループして処理
 music_array.each do |music|
   puts music
   `afplay -q 1 #{music.shellescape}`
 end  
 
 # 課題
-# 音楽ファイルのみを実行対象とすること
 # 外部割り込みで次の楽曲を演奏できるようにすること
+# 演奏時間を表示すること
