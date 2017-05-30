72d7c34737b8fbaf0e5dcdf3a7b12f7a80df09b3
k-yoshiyan <yoshiyan@home.email.ne.jp>
2017-05-30 21:11:25 +0900

    Update afp.rb
    パラメータで数字を与えた場合その数の曲数から演奏を開始すること

--- afp.rb
+++ afp.rb
@@ -8,6 +8,9 @@ path = Dir.pwd
 extensions = [".mp3", ".wav", ".MP3", ".WAV"]
 music_array = []
 
+## パラメーターで開始位置を与えられた場合に変数にセット
+if ARGV[0] then start_point = ARGV[0].to_i - 1  else start_point = 0 end
+
 ## ディレクトリーの内容オブジェクト数分ループして処理をし、演奏楽曲ファイル名の配列を作る
 Dir.foreach(path) do |x|
 ### mp3, wavのファイル名のみを選択する　
@@ -16,12 +19,14 @@ Dir.foreach(path) do |x|
   end
 end
 
+
 ## 演奏楽曲の配列を個数分ループして処理
-music_array.each do |music|
+## dropメソッドでパラメータ分の配列要素を除去
+music_array.drop(start_point).each do |music|
   puts music
   `afplay -q 1 #{music.shellescape}`
 end  
 
 # 課題
-# 外部割り込みで次の楽曲を演奏できるようにすること
+# ディレクトリー内の全ファイルの表示と再生中ファイル名を表示すること
 # 演奏時間を表示すること
