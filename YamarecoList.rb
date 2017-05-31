bc18a79a18ca5d08443d84ef26ce56ec07bb0129
k-yoshiyan <yoshiyan@home.email.ne.jp>
2017-06-01 00:24:33 +0900

    Update YamarecoList.rb
    ページのハンドリング。ループをエラーで抜けるようにした

--- YamarecoList.rb
+++ YamarecoList.rb
@@ -9,7 +9,7 @@ Net::HTTP.version_1_2
 
 Host = 'www.yamareco.com'
 if ARGV[0] then userID = ARGV[0].to_s else userID= '4541' end
-maxpage = 14
+maxpage = 20
 
 (1..maxpage).each do |page|
   path = '/api/v1/getReclist/user/' + userID + '/' + page.to_s
@@ -17,14 +17,17 @@ maxpage = 14
   Net::HTTP.start(Host, 80) do |http|
     response = http.get(path)
     js = JSON.parse(response.body)
-
-    js["reclist"].each do |rec|
+    if js["err"] == 1 then break
+    else
+      js["reclist"].each do |rec|
       puts rec["start"] + ' : ' + rec["place"] + '('  + rec["rec_id"] + ')'
-    end
+      end
+    end  
   end
 end
 
 #課題
-#　ページのハンドリング。ループをエラーで抜けるようにする
+#　デフォルトをなしにして公開
+
 
 
