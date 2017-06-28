6e39441992a72a910f323f05a15734edc9db44ff
k-yoshiyan <yoshiyan@home.email.ne.jp>
2017-06-28 21:32:47 +0900

    Update afprand.rb
    特定ディレクトリーの下を再起的に演奏対象とする

--- afprand.rb
+++ afprand.rb
@@ -1,36 +1,35 @@
 #!/usr/bin/ruby
 # coding: utf-8
 
-# afp.rb
-# カレントディレクトリーの下のファイルをafplayでランダム再生する
+# afprand.rb
+# wavdata の下をafplayでランダム再生する
 require 'shellwords'
-path = Dir.pwd
 extensions = [".mp3", ".wav", ".MP3", ".WAV"]
+
+directory = '/Users/koji/Music/wavdata'
+dir_array = []
 music_array = []
 
-## パラメーターで開始位置を与えられた場合に変数にセット
-if ARGV[0] then start_point = ARGV[0].to_i - 1  else start_point = 0 end
-
-## ディレクトリーの内容オブジェクト数分ループして処理をし、演奏楽曲ファイル名の配列を作る
-Dir.foreach(path) do |x|
-### mp3, wavのファイル名のみを選択する　
+Dir.chdir(directory)
+dir_array = Dir.glob('./**/*')
+dir_array.each do |x|
   if extensions.include?(File.extname(x))
-    music_array.push(x)
+    music_array.push(directory + x[1..300])
   end
 end
+puts music_array.size
 
 loop do
 ## 配列からランダムに再生楽曲を選択する
 music = music_array[rand(music_array.size)]
 
 ## 演奏対象ファイル名の表示と演奏
-puts "(#{music_array.index(music)+1}) " + music
-puts 
+puts music
 puts "  Start: " + Time.now.strftime("%X") + "  Size: " + File.stat(music).size.div(1024).to_s + "kb"
   `afplay -q 1 #{music.shellescape}`
-
+puts 
 end
 
 # 課題
-# 特定ディレクトリーの下の楽曲をすべて配列にいれてランダム演奏する
+# wavdata か mp3data かを選択可能にする
 
