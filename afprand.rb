081e38d10a1f3d79b63c5bb1346ed6c4eb858c15
k-yoshiyan <yoshiyan@home.email.ne.jp>
2020-05-03 16:47:15 +0900

    soxコマンドによる演奏時間の表示、flac, m4aの追加

--- afprand.rb
+++ afprand.rb
@@ -4,10 +4,11 @@
 # afprand.rb
 # wavdata の下をafplayでランダム再生する
 require 'shellwords'
-extensions = [".mp3", ".wav", ".MP3", ".WAV"]
+extensions = [".mp3", ".wav", ".MP3", ".WAV", ".m4a", ".flac"]
 
-directory = '/Users/koji/Music/wavdata'
+directory = '/Volumes/wavdata'
 if ARGV[0] == "-p" then directory = '/Users/koji/Music/mp3data' end
+if ARGV[0] == "-pp" then directory = '/Users/koji/Music/mp3-sync' end
 dir_array = []
 music_array = []
 
@@ -26,11 +27,13 @@ music = music_array[rand(music_array.size)]
 
 ## 演奏対象ファイル名の表示と演奏
 puts music
-puts "  Start: " + Time.now.strftime("%X") + "  Size: " + File.stat(music).size.div(1024).to_s + "kb"
+  puts "  Start: " + Time.now.strftime("%X") 
+  music_time = `sox --i -d #{music.shellescape}`
+  puts "  Time:  " + music_time
   `afplay -q 1 #{music.shellescape}`
 puts 
 end
 
 # 課題
-
+# wavdata か mp3data かを選択可能にする
 
