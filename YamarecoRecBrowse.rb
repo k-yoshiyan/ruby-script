d14be0b897b9ed9be8183023088468bf36874c28
k-yoshiyan <yoshiyan@home.email.ne.jp>
2021-10-18 18:55:14 +0900

    Windows環境でのコマンド追加

--- YamarecoRecBrowse.rb
+++ YamarecoRecBrowse.rb
@@ -7,6 +7,8 @@ if ARGV[0] != nil then recID = ARGV[0].to_s
 end
 
 `open -a "/Applications/Firefox.app" https://www.yamareco.com/modules/yamareco/detail-#{recID}.html`
+# Windows
+# `rundll32.exe url.dll,FileProtocolHandler  https://www.yamareco.com/modules/yamareco/detail-#{recID}.html` 
 
 #課題
 # 標準入力からレコードIDを渡したい
